4387951821de92c6725f3753a3c37e12321a9ada
Suzie Brown <suzie@Suzies-MBP.connect>
2018-07-04 14:12:59 +0100

    Updated plot appearance (and fixed function for p^k)

--- R_test/eg_runtime_plots.R
+++ R_test/eg_runtime_plots.R
@@ -1,11 +1,23 @@
-## Make plots of expected running time for example Bernoulli factories
+# Make plots of expected running time for example Bernoulli factories
 
 ## Von Neumann f(p)=1/2
-p <- seq(from = 0.1, to = 0.9, by = 0.01)
-plot(p, 1/(p*(1-p)), type='l', lwd=2, ylim=c(1,11), col='blue', ylab='E(tau)', main="Expected running time of Von Neumann's Bernoulli factory")
+p <- seq(from = 0.05, to = 0.95, by = 0.01)
+plot(p, 1/(p*(1-p)), type='l', ylim=c(1,22), lwd=2, ylab='E(tau)', main="Expected running time of Von Neumann's Bernoulli factory")
 
-## f(p)=p^k
+## f(p)=p^k -------------
+## single plot
 p <- seq(from = 0, to = 1, by = 0.01)
-k <- 2
-fp <- -p^k + (k+1)*p^(2*k) + (1-p^k)*(1-p^(k-1))/(1-p)
+k <- 3
+fp <- -p^k + (1-p^(k+1))/(1-p)
+if (k == 1) {fp <- rep(1, length(p))}
 plot(p, fp, type='l', lwd=2, col='blue', ylim=c(0,k), ylab='E(tau)', main=paste("Expected running time of p^",k," Bernoulli factory", sep=""))
+
+## overlay several plots
+kvals <- 1:5
+plot(NA, xlim = c(0, 1), ylim = c(1, max(kvals)), ylab = "E(tau)", xlab = "p", main = "Expected running time of p^k Bernoulli factory")
+for (k in kvals) {
+  fp <- -p^k + (1-p^(k+1))/(1-p)
+  if (k == 1) {fp <- rep(1, length(p))}
+  lines(p, fp , lty = k, lwd = 2)
+}
+legend("topleft", paste("k=",kvals), lwd = 2, lty = kvals)
