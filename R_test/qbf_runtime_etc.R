9f141aade8323faf3c881abd91a3756aed3262d7
suziebrown <suzie.brown42@gmail.com>
2018-08-31 15:25:40 +0100

    renamed q to qk

--- R_test/qbf_runtime_etc.R
+++ R_test/qbf_runtime_etc.R
@@ -8,7 +8,7 @@ plot(p, f(p), type='l', lwd=2, ylim=c(0,200))
 
 qk <- function(k) {choose(2*k,k)/((2*k-1)*2^(2*k))}
 k <- 1:100
-plot(k, q(k), log='y', ylab='q_k', pch=16)
+plot(k, qk(k), log='y', ylab='q_k', pch=16)
 
 #---
 # running time of QBF for wedge fn
@@ -30,8 +30,8 @@ for (j in 1:length(p2)) {
 }
 
 plot(p, Et, type='l', lwd=2, ylim=c(0,20), ylab='E(tau)')
-#plot(p, rep(sum(2*k*q(k)), length(p)))
-#plot(k, cumsum(2*k*q(k)))
+#plot(p, rep(sum(2*k*qk(k)), length(p)))
+#plot(k, cumsum(2*k*qk(k)))
 
 
 
@@ -42,7 +42,7 @@ qk <- function(k) {choose(2*k,k)/((2*k-1)*2^(2*k))}
 p <- 0.49
 p2 <- function(p) {4*p*(1-p)}
 k <- 1:500
-kterms <- q(k)
+kterms <- qk(k)
 diff <- 1 - cumsum(kterms)
 plot(k, diff, xlab='k_max', ylab='error', log='y')
 
@@ -50,23 +50,24 @@ plot(k, diff, xlab='k_max', ylab='error', log='y')
 qk <- function(k) {choose(2*k,k)/((2*k-1)*2^(2*k))}
 k <- 1:500
 
-diff <- 1- cumsum(q(k))
-change_error <- q(k)
-quoins <- (2/(1-p2(p)))* cumsum(q(k)*(1-p2(p)^k))
+diff <- 1- cumsum(qk(k))
+change_error <- qk(k)
+quoins <- (2/(1-p2(p)))* cumsum(qk(k)*(1-p2(p)^k))
 #plot(k, diff)
 #plot(k, quoins)
-ratio <- diff/quoins
-plot(k,ratio, log='y')
-lines(0.5/k, lwd=2, col='red')
+ratio <- quoins/diff
+plot(k,ratio)
+lines(1.952*k, lwd=2, col='red')
+#lines(0.05*k^(-0.5), lwd=2, col='purple') ## nope, that deffo doesn't fit
 
 
-# target function wehn k is restricted
+### target function wehn k is restricted
 kmax <- 500
 k <- 1:kmax
 p <- seq(0.01, 0.99, 0.01)
 target <- numeric(length(p))
 for (i in 1:length(p)) {
-  target[i] <- sum(q(k)*p2(p[i])^k)
+  target[i] <- sum(qk(k)*p2(p[i])^k)
 }
 #plot(p, target, type='l', lwd=2, ylim=c(0,1), ylab = 'approximate target')
 #lines(p, target, lwd=2)
